c39a25b4d4d353fcb1aa5964551df4795c467a99
Park Hyunho <lumenize@ajou.ac.kr>
2024-03-18 17:27:18 +0900

    Practice2

---
+++ FirstProgram/_Practice2/Program.cs
@@ -0,0 +1,33 @@
+﻿/// 2-1
+/// 숫자의 사칙연산
+{
+    int ten = 10;
+
+    int result_1 = ten + 7;
+    int result_2 = ten - 3;
+    int result_3 = ten * 2;
+    float result_4 = ten * 1.5f;
+    float result_5 = ten / 3;
+    int result_6 = ten % 4;
+}
+
+/// 2-2
+/// 문자의 계산
+{
+    string name = "현호"; // 자신의 이름, 닉네임 으로 연습해보세요.
+    int year = 2023;
+
+    string introduce = "안녕하세요 제 이름은 \"" + name + "\" 입니다." ; // 안녕하세요. 제 이름은 "chad" 입니다.
+    string thisYear = "올해는 \'" + year + "\' 입니다."; // 올해는 '2023년' 입니다.
+}
+
+/// 2-3
+/// 논리 연산
+{
+    int ten = 10;
+
+    bool result_1 = ten == 10;
+    bool result_2 = ten != 10;
+    bool result_3 = ten <= 20;
+    bool result_4 = ten >= 5;
+}
